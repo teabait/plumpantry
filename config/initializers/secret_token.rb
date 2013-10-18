6375a1e17876609b61f8e5ccb857ce632555e3e7
teabait <hello.teaho@gmail.com>
2013-10-18 17:18:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlumPantry::Application.config.secret_key_base = 'a1928dbd66460979233172cf525384f04b0da65f846cca80eeff1a166e84dbdcd1a2ec033743c06a4c9c1ad3a6d77499f905bed42011efa2c7802a2fc91511cc'
