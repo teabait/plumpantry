60352e9202ccc5efb4f6ba1bf54d69cd38ac5b99
teabait <hello.teaho@gmail.com>
2013-10-18 17:47:44 -0400

    yummly configuration

---
+++ config/initializers/yummly_api.rb
@@ -0,0 +1,7 @@
+# configure Yummly with your APP key and ID
+
+Yummly.configure do |config|
+  config.app_id = "c74f5216"
+  config.app_key = "2da3cbe5c3a5f5ab12c47de4e71f19a5"
+  config.use_ssl = true # Default is false
+end
