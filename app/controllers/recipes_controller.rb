ab697b64057da47ad807eb10c7858c978531ce07
teabait <hello.teaho@gmail.com>
2013-10-21 14:49:34 -0400

    users form, users new

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,8 @@
+class RecipessController < ApplicationController
+  def new
+    render :new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 PlumPantry::Application.routes.draw do
   resources :users, only: [:new, :create, :show]
-  resources :recipes, only [:new, :create,:index, :show]
+  resources :recipes, only: [:new, :create,:index, :show]
 end
