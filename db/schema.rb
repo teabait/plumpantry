2630dfa0d99090058776e09998477d3aaef1e14c
teabait <hello.teaho@gmail.com>
2013-10-21 14:37:44 -0400

    recipes_users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131021180154) do
+ActiveRecord::Schema.define(version: 20131021183619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20131021180154) do
     t.json     "yummly_object", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "recipes_users", force: true do |t|
+    t.integer "user_id",   null: false
+    t.integer "recipe_id", null: false
   end
 
   create_table "users", force: true do |t|
