5c038eebbf694781ff43f7394a017159735d36cc
teabait <hello.teaho@gmail.com>
2013-10-22 15:51:27 -0400

    working seeds, modified recipe for errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ActiveRecord::Base
   validates :yummly_id, presence: true
-  has_and_belongs_to_many :users, :ingredients
+
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :ingredients
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
+
 Yummly.search('fall', params={'maxResult'=>100 }).each do |recipe|
+    
     @yummly_id = recipe.id
     @yummly_recipe_name = recipe.recipe_name
-    @yummly_image = recipe.images[0].small_url
-    yummly_recipe = YummlyRecipe.new(name: @yummly_recipe_name, yummly_id: @yummly_id, image: @yummly_image)
+    @yummly_image = recipe.images[0].small_url unless recipe.images[0].nil?
+    yummly_recipe = Recipe.new(name: @yummly_recipe_name, yummly_id: @yummly_id, image: @yummly_image)
 
-    recipe.ingredients.each do |i|
-      ingredient = Ingredient.find_or_initalize_by(name: i)
-      yummly_recipe.ingredients << ingredient
+    unless recipe.ingredients.nil?
+      recipe.ingredients.each do |i|
+        ingredient = Ingredient.find_or_initialize_by(name: i)
+        yummly_recipe.ingredients << ingredient
+      end
     end
 
     yummly_recipe.save
