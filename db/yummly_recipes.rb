ffbf966db87bf9fabac2cafec750d546e62ac4c4
teabait <hello.teaho@gmail.com>
2013-10-22 14:50:57 -0400

    delete yummly_recipes.rb

--- db/yummly_recipes.rb
+++
@@ -1,7 +0,0 @@
-Yummly.search('fall', maxResult=100).each do |recipe|
-    @yummly_id = recipe.recipe_id
-    @yummly_recipe_name = recipe.recipe_name
-    yummly_recipe = YummlyRecipe.new(yummly_name: "#{@yummly_recipe_name}", yummly_id: "#{@yummly_id}")
-    yummly_recipe.save
-    end
-  end
