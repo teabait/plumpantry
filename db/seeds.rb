77b556b0660cc87fc81c1acacdb80d7b13b0d6ee
teabait <hello.teaho@gmail.com>
2013-10-23 14:45:45 -0400

    recipes doesn't display duplicates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 
-Yummly.search('dessert', params={'maxResult'=>100 }).each do |recipe|
+Yummly.search('smoothie', params={'maxResult'=>100 }).each do |recipe|
     
     @yummly_id = recipe.id
     @yummly_recipe_name = recipe.recipe_name
     @yummly_image = recipe.images[0].small_url unless recipe.images[0].nil?
-    yummly_recipe = Recipe.new(name: @yummly_recipe_name, yummly_id: @yummly_id, image: @yummly_image)
+    yummly_recipe = Recipe.find_or_initialize_by(name: @yummly_recipe_name, yummly_id: @yummly_id, image: @yummly_image)
 
     unless recipe.ingredients.nil?
       recipe.ingredients.each do |i|
