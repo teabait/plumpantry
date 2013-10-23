278fac12e8b84b07ea9fb21395e924d03dd1bdcf
teabait <hello.teaho@gmail.com>
2013-10-23 14:20:48 -0400

    Show similar dishes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,21 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @ingredients = @recipe.ingredients
+    # That recipe's ingredients into a searchable array
+    @ingredients_array = @ingredients.map do |i|
+      i.id
+    end
+
+    @recipes = Recipe.all
+    @similar = @recipes.select do |r|
+      (@r_ingredients = r.ingredients.map do |i|
+        i.id
+      end)
+       (@r_ingredients & @ingredients_array).size >= 3
+    end
+
+
     render :show
   end
 end
