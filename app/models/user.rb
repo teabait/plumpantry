f212cd5f1270dceaab152e69b9732d2c64f17a54
teabait <hello.teaho@gmail.com>
2013-10-23 19:47:58 -0400

    user ability to add recipe to fave from individual recipe page; show only if user is logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   has_and_belongs_to_many :recipes
   has_secure_password
+
+  def favorites
+    self.recipes.map do |r|
+      r.name
+    end
+  end
 end
