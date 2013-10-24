661b15e6c5f0c50d95742f0e497fbd101a0f133d
teabait <hello.teaho@gmail.com>
2013-10-24 12:16:23 -0400

    In the middle of cleaning similar recipe code, where everything works

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,21 +13,9 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.ingredients
-    # That recipe's ingredients into a searchable array
-    @ingredients_array = @ingredients.map do |i|
-      i.id
-    end
 
     @recipes = Recipe.all
 
-    @similar = @recipes.select do |r|
-      #set array of ingredient ids to @r_ingredients
-      @r_ingredients = r.ingredients.map do |i|
-        i.id
-      end
-      #find intersection of @r_ingredients to this recipe's ingredients
-       (@r_ingredients & @ingredients_array).size >= 3
-    end
     render :show
   end
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,20 @@ class Recipe < ActiveRecord::Base
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :ingredients
+
+  def similar
+    recipes = Recipe.all
+    ingredients = self.ingredients
+    @ingredients_array = ingredients.map do |i|
+      i.id
+    end
+    @similar = recipes.select do |r|
+      #set array of ingredient ids to @r_ingredients
+      @r_ingredients = r.ingredients.map do |i|
+        i.id
+      end
+      #find intersection of @r_ingredients to this recipe's ingredients
+       (@r_ingredients & @ingredients_array).size >= 3
+    end
+  end
 end
