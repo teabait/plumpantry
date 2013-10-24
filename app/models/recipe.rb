d2280bd081a4c0a86b37d523ec58df3a19efb330
teabait <hello.teaho@gmail.com>
2013-10-24 16:05:37 -0400

    Large Image Hack

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -19,4 +19,20 @@ class Recipe < ActiveRecord::Base
      end
     end
   end
+
+  def large_image
+    last_four = self.image.split(//).last(4).join("").to_s
+    last_five = self.image.split(//).last(5).join("").to_s
+    if last_four == "=s90"
+      new_end = last_four.gsub('=s90','=l180')
+      sliced = self.image[0..-5]
+    elsif last_five == "s.jpg"
+      new_end = last_five.gsub('s.jpg','l.jpg')
+      sliced = self.image[0..-6]
+    elsif last_five == "s.png"
+      sliced = self.image[0..-6]
+      new_end = last_five.gsub('s.png','l.png')
+    end
+    sliced + new_end
+  end
 end
