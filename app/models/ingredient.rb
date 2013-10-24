97d10b92c9671dd5e0db54c246558be389f92a27
teabait <hello.teaho@gmail.com>
2013-10-24 11:16:20 -0400

    ingredient model method for recipe suggestions

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,13 @@
 class Ingredient < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
     has_and_belongs_to_many :recipes
+
+    def recipe_suggest
+      self.recipes.each do |r|
+        suggested_recipes = []
+        suggested_recipes << r
+        suggested_recipes.uniq!
+      end
+   end
 end
+
